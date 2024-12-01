a8aa8eef58f89637dc9bc546609b16753725eed5
Joan <j.ricartt@gmail.com>
2024-12-01 20:51:36 +0100

    wip part 2

--- day_1/lib/day1.ex
+++ day_1/lib/day1.ex
@@ -1,5 +1,5 @@
 defmodule Day1 do
-  def main do
+  def part1 do
     # read line by line splitting the input on newlines
     # trim the last element because its empty
     pairs =
@@ -27,6 +27,31 @@ defmodule Day1 do
         Kernel.abs(Enum.at(first, x) - Enum.at(second, x))
       end)
       |> Enum.sum()
+
+    IO.inspect(result)
+  end
+
+  def part2 do
+    # read line by line splitting the input on newlines
+    # trim the last element because its empty
+    pairs =
+      File.read!("input.txt")
+      |> String.split("\n")
+      |> Enum.map(&String.split/1)
+      |> Enum.reverse()
+      |> tl()
+      |> Enum.reverse()
+
+    [first, second] =
+      Enum.reduce(pairs, [[], []], fn pair, list ->
+        [
+          Enum.at(list, 0) ++ [String.to_integer(Enum.at(pair, 0))],
+          Enum.at(list, 1) ++ [String.to_integer(Enum.at(pair, 1))]
+        ]
+      end)
+      |> Enum.map(&Enum.sort/1)
+
+    Enum.each(first, fn item -> nil end)
 
     IO.inspect(result)
   end
